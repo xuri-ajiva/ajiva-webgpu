7a6ea3e4d6dac2ee63f0bc702599ebd993cea11b
Tobias Durst <t0bl4s.durst@gmail.com>
2023-08-11 21:47:52 +0200

    Added GLM

--- src/TestBed/main.cpp
+++ src/TestBed/main.cpp
@@ -1,8 +1,12 @@
 
 
 
-#include "library.h"
 #include <iostream>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_LEFT_HANDED
+
+#include <glm/ext.hpp>
 
 #define WEBGPU_CPP_IMPLEMENTATION
 
